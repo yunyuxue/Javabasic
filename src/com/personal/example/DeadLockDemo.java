fd28845ca0a474421e6d16a6ad5dd42034baa3e7
AlaneyS <yunyuxue521@163.com>
2019-07-01 16:39:17 +0800

    更新死锁demo

--- src/com/personal/example/DeadLockDemo.java
+++ src/com/personal/example/DeadLockDemo.java
@@ -10,48 +10,43 @@ package com.personal.example;
 public class DeadLockDemo {
 
     public static void main(String[] args){
-        //两个信号量
-        String signalA = "A";
-        String signalB = "B";
+        final String signalA = "A";
+        final String signalB = "B";
 
-        //每个线程拥有对方需要的信号且不放手，并需要对方拥有的信号；
-        Thread ta = new Thread(new Runnable(){
-        @Override
-        public void run(){
-            System.out.println("in thread A body..");
-            synchronized(signalA){
-                try{
-                    Thread.sleep(1000);
-                    System.out.println("hold signal A..");
-                } catch (InterruptedException e){
-                    e.printStackTrace();
-                }
-                synchronized(signalB){
-                    System.out.println("need signal B in Thread A body..");
-                }
-            }
-        }
-    });
-
-        Thread tb = new Thread(new Runnable(){
+        //holding signalA, needs signalB to entrant
+        new Thread(new Runnable(){
             @Override
             public void run(){
-                System.out.println("in thread B body..");
+                synchronized(signalA){
+                    try{
+                        System.out.println("In " + Thread.currentThread().getName() + " body, holding signalA...");
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                    synchronized(signalB){
+                        System.out.println("In " + Thread.currentThread().getName() + " body, holding signalB...");
+                    }
+                }
+            }
+        }).start();
+
+        //holding signalB, needs signalA to entrant
+        new Thread(new Runnable(){
+            @Override
+            public void run(){
                 synchronized(signalB){
                     try{
+                        System.out.println("In " + Thread.currentThread().getName() + " body, holding signalB...");
                         Thread.sleep(1000);
-                        System.out.println("hold signal B..");
                     } catch (InterruptedException e){
                         e.printStackTrace();
                     }
                     synchronized(signalA){
-                        System.out.println("need signal A in Thread B body..");
+                        System.out.println("In " + Thread.currentThread().getName() + " body, holding signalA...");
                     }
                 }
             }
-        });
-
-        ta.start();
-        tb.start();
+        }).start();
     }
 }
