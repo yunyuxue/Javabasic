97c74d75a86fb618cd77db1a71bbe4747da62018
AlaneyS <yunyuxue521@163.com>
2019-07-01 17:44:44 +0800

    Lambda表达式实现线程死锁

---
+++ src/com/personal/example/LambdaDeadLockDemo.java
@@ -0,0 +1,48 @@
+package com.personal.example;
+
+/**
+ * @author ：song
+ * @date ：Created in 2019/6/3 17:15
+ * @description：实现死锁, lambda实现
+ * @modified By：
+ * @version: 1.0.0
+ */
+public class LambdaDeadLockDemo {
+
+    public static void main(String[] args){
+        final String signalA = "A";
+        final String signalB = "B";
+
+        //holding signalA, needs signalB to entrant
+        new Thread(()-> {
+                            synchronized(signalA){
+                                try{
+                                    System.out.println("In " + Thread.currentThread().getName() + " body, holding signalA...");
+                                    Thread.sleep(1000);
+                                } catch (InterruptedException e){
+                                    e.printStackTrace();
+                                }
+                                synchronized(signalB){
+                                    System.out.println("In " + Thread.currentThread().getName() + " body, holding signalB...");
+                                }
+                            }
+                        }
+                    ).start();
+
+        //holding signalB, needs signalA to entrant
+        new Thread(()-> {
+                            synchronized(signalB){
+                                try{
+                                    System.out.println("In " + Thread.currentThread().getName() + " body, holding signalB...");
+                                    Thread.sleep(1000);
+                                } catch (InterruptedException e){
+                                    e.printStackTrace();
+                                }
+                                synchronized(signalA){
+                                    System.out.println("In " + Thread.currentThread().getName() + " body, holding signalA...");
+                                }
+                            }
+                        }
+                ).start();
+    }
+}
