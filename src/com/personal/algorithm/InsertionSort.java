5d7b4e0a2c84a4dcf04fd5cb2ca32e2fd5269c53
AlaneyS <yunyuxue521@163.com>
2019-07-03 10:28:12 +0800

    添加插入排序

---
+++ src/com/personal/algorithm/InsertionSort.java
@@ -0,0 +1,29 @@
+package com.personal.algorithm;
+
+/**
+ * @Author AlaneyS
+ * @Date 2019/7/3 10:06
+ * @Description 插入排序： 时间--最差、平均-O(N^2), 最好-O(N), 空间-O(1)
+ * @Modified By
+ * @Version: 1.0.0
+ **/
+public class InsertionSort {
+
+    public static void sort(Comparable[] c){
+        int N = c.length;
+        for(int i = 1; i < N; i++){
+            for(int j = i; j > 0 && SortUtils.less(c[j],c[j-1]); j--){
+                SortUtils.swap(c, j, j-1);
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        String[] strs = SortUtils.generateStrArray(12, 3);
+        System.out.println("------before sort------");
+        SortUtils.show(strs);
+        sort(strs);
+        System.out.println("------after ascend sort------");
+        SortUtils.show(strs);
+    }
+}
